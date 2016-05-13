91f393673b3c2739682dfc1b2140381bb71d9422
jackie <scottfield@qq.com>
2016-05-13 16:07:50 +0800

    configure log4j

--- hibernate/src/test/java/com/jackie/hibernate/service/UserServiceTest.java
+++ hibernate/src/test/java/com/jackie/hibernate/service/UserServiceTest.java
@@ -2,15 +2,12 @@ package com.jackie.hibernate.service;
 
 import com.jackie.hibernate.model.User;
 import org.apache.log4j.Logger;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.util.Log4jConfigurer;
 
 import javax.annotation.Resource;
-import java.io.FileNotFoundException;
 
 /**
  * Created by jackie on 5/13/2016.
@@ -18,11 +15,6 @@ import java.io.FileNotFoundException;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(value = {"classpath:/applicationcontext.xml"})
 public class UserServiceTest {
-    @BeforeClass
-    public static void init() throws FileNotFoundException {
-        Log4jConfigurer.initLogging("classpath:log4.properties");
-    }
-
     @Resource
     private UserService userService;
     private Logger logger = Logger.getLogger(UserServiceTest.class);
@@ -34,5 +26,6 @@ public class UserServiceTest {
         user.setUsername("jackie");
         user.setPassword("password");
         userService.addUser(user);
+
     }
 }
