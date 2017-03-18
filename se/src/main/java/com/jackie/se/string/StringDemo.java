645f6b9acc1db53d673545c593c62d95c1d6838c
scottfield <scottfield@qq.com>
2017-03-18 13:49:25 +0800

    添加字符串类的演示demo

---
+++ se/src/main/java/com/jackie/se/string/StringDemo.java
@@ -0,0 +1,16 @@
+package com.jackie.se.string;
+
+/**
+ * Title(文件名): StringDemo<p>
+ * Description(描述):<p>
+ * Copyright(版权): Copyright (c) 2016<p>
+ * Company(公司): 成都四方伟业软件股份有限公司<p>
+ * CreateTime(生成日期):2017/3/18
+ *
+ * @author SF-2171
+ */
+public class StringDemo {
+    public static void main(String[] args) {
+        System.out.println("你好"+"吗");
+    }
+}
